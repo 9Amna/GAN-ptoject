f1d35da78e93c5d9a59dc48775de3f816db996b9
Amna_Bouaine <emna.bouaine@etudiant-enit.utm.tn>
2022-08-10 09:23:18 +0100

    fix : betas value

--- train.py
+++ train.py
@@ -363,15 +363,15 @@ if __name__ == "__main__":
     train_dl = DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True, drop_last=True)
     val_dl = DataLoader(val_ds, batch_size=BATCH_SIZE, shuffle=False, drop_last=False)
 
-    # resume = True
-    # if resume:
-    #     # load pretrained models D & G
-    #     G = Generator()
-    #     G.load_state_dict(torch.load(f"/content/drive/MyDrive/saving_G30.pth"))
-    #     D = Discriminator()
-    #     D.load_state_dict(torch.load(f"/content/drive/MyDrive/saving_D30.pth"))
-    # else:
-    #     G = Generator()
-    #     D = Discriminator()
+    resume = True
+    if resume:
+         # load pretrained models D & G
+         G = Generator()
+         G.load_state_dict(torch.load(f"/content/drive/MyDrive/saving_G30.pth"))
+         D = Discriminator()
+         D.load_state_dict(torch.load(f"/content/drive/MyDrive/saving_D30.pth"))
+    else:
+         G = Generator()
+         D = Discriminator()
     # EPOCH = 25
     # trained_G, trained_D = train_loop(train_dl, G, D, EPOCH)
