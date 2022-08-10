ef2c89852460e9af36f9f2e8e912caf39896daa3
Amna_Bouaine <emna.bouaine@etudiant-enit.utm.tn>
2022-08-10 17:52:38 +0100

    add : device parameter

--- train.py
+++ train.py
@@ -159,7 +159,7 @@ class Discriminator(nn.Module):
         return self.layer6(out)  # (b, 1, 2, 2)
 
 
-def train_fn(train_dl, G, D, criterion_bce, criterion_mae, optimizer_g, optimizer_d,device):
+def train_fn(train_dl, G, D, criterion_bce, criterion_mae, optimizer_g, optimizer_d, device):
     G.train()
     D.train()
     LAMBDA = 100.0
@@ -168,8 +168,8 @@ def train_fn(train_dl, G, D, criterion_bce, criterion_mae, optimizer_g, optimize
         input_img = input_img.to(device)
         real_img = real_img.to(device)
 
-        real_label = torch.ones(input_img.size()[0], 1, 8, 8)
-        fake_label = torch.zeros(input_img.size()[0], 1, 8, 8)
+        real_label = torch.ones(input_img.size()[0], 1, 8, 8).to(device)
+        fake_label = torch.zeros(input_img.size()[0], 1, 8, 8).to(device)
         # Generator
         fake_img = G(input_img)
         fake_img_ = fake_img.detach()  # commonly using
