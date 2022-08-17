fa4083b35110154a312cb521ace6779beffb4d93
Amna_Bouaine <emna.bouaine@etudiant-enit.utm.tn>
2022-08-17 10:03:15 +0100

    modify : resize value

--- train.py
+++ train.py
@@ -205,8 +205,8 @@ def train_fn(train_dl, G, D, criterion_bce, criterion_mae, optimizer_g, optimize
         input_img = input_img.to(device)
         real_img = real_img.to(device)
 
-        real_label = torch.ones(input_img.size()[0], 1, 8, 8).to(device)
-        fake_label = torch.zeros(input_img.size()[0], 1, 8, 8).to(device)
+        real_label = torch.ones(input_img.size()[0], 1, 16, 16).to(device)
+        fake_label = torch.zeros(input_img.size()[0], 1, 16, 16).to(device)
         # Generator
         fake_img = G(input_img)
         fake_img_ = fake_img.detach()  # commonly using
